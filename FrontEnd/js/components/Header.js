540ff16427fe9130f55c7b979b16d77189c69d4b
Vincent-Omondi <vincentomondi251@gmail.com>
2025-02-27 08:12:26 +0300

    Updated Header: Dynamically updates inbox notification count

--- FrontEnd/js/components/Header.js
+++ FrontEnd/js/components/Header.js
@@ -1,7 +1,11 @@
+import messageStore from '../store/messageStore.js';
+
 export class Header {
     constructor() {
         this.element = document.createElement('header');
         this.element.className = 'header';
+        this.messageStore = messageStore;
+        this.unsubscribe = messageStore.subscribe(() => this.updateUnreadCount());
     }
 
     render() {
@@ -26,13 +30,43 @@ export class Header {
                     <span class="button-text">New Post</span>
                 </a>
             </div>
-            <a href="/messages" class="nav-link">
-                <i class="fas fa-envelope"></i>
-            </a>
+            <div class="messages-icon">
+                <a href="/messages" data-link>
+                    <i class="fas fa-envelope"></i>
+                    <span class="message-count"></span>
+                </a>
+            </div>
             <div id="userSection" class="profile-section">
                 <!-- User section will be dynamically populated -->
             </div>
         `;
+
+        // Update count after the element is created
+        this.updateUnreadCount();
         return this.element;
+    }
+
+    updateUnreadCount() {
+        const badge = document.querySelector('.messages-badge');
+        const count = messageStore.getTotalUnreadCount();
+        
+        if (count > 0) {
+            if (!badge) {
+                const newBadge = document.createElement('span');
+                newBadge.className = 'messages-badge';
+                newBadge.textContent = count;
+                document.querySelector('.messages-icon').appendChild(newBadge);
+            } else {
+                badge.textContent = count;
+            }
+        } else if (badge) {
+            badge.remove();
+        }
+    }
+
+    destroy() {
+        if (this.unsubscribe) {
+            this.unsubscribe();
+        }
     }
 } 
